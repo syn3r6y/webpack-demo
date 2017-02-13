92794a060920aefb30057420a052f93b3ff4ae0a
syn3r6y <syn3rgy@live.ca>
2017-02-13 08:40:56 -0700

    Defines config for Hot Module Reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 const PATHS = {
     app: path.join (__dirname, 'app'),
     build: path.join(__dirname, 'build'),
 };
 
-module.exports = {
+const common = {
     //entry accepts a path of an object of entries
     entry:{
         app: PATHS.app,
@@ -20,4 +21,43 @@ module.exports = {
             title: 'Webpack demo'
         })
     ],
+};
+
+function production(){
+    return common;
+}
+
+function development(){
+    const config = {
+        devServer:{
+            //HTML5 History API-based routing
+            historyApiFallback: true,
+            //don't refresh if HMR fails
+            hotOnly: true,
+            //display only errors
+            stats: 'errors-only',
+            host: process.env.HOST, //defaults to localhost
+            port: process.env.PORT, //defaults to 8080
+        },
+        plugins:[
+            new webpack.HotModuleReplacementPlugin(),
+        ],
+    };
+
+    return Object.assign(
+        {},
+        common,
+        config,
+        {
+            plugins: common.plugins.concat(config.plugins),
+        }
+    );
+}
+
+module.exports = function(env){
+    if(env === 'production'){
+        return production();
+    }
+
+    return development();
 };
